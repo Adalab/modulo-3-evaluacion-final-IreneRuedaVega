63322e4f64d582eb7b59b938e84c885911c192e4
IreneRuedaVega <ruedavega.irene@gmail.com>
2020-11-23 17:32:30 +0100

    Get data from amplete

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../stylesheets/App.scss";
 import getDataFromApi from "../services/getDataFromApi";
 
-console.log(getDataFromApi());
-
 const App = () => {
+  //estados
+  const [characters, setCharacters] = useState([]);
   useEffect(() => {
-    console.log("Me estoy montando");
-    getDataFromApi();
-  });
+    /*   console.log("Me estoy montando"); */
+    getDataFromApi().then((data) => {
+      /*  console.log(data); */
+      setCharacters(data);
+    });
+  }, []); // con este array vacío le decimos a React que solo ejecute este useEffect una vez. Ahora solo pedimos los datos al arrancar.
+
+  /*  console.log(characters); */
+
   return <></>;
 };
 
--- src/services/getDataFromApi.js
+++ src/services/getDataFromApi.js
@@ -1,7 +1,23 @@
 const getDataFromApi = () => {
-  return fetch("https://rickandmortyapi.com/api/character/").then((response) =>
-    response.json()
-  );
+  return fetch("https://rickandmortyapi.com/api/character/")
+    .then((response) => response.json())
+    .then((data) => {
+      //Ya tengo el array que puedo iterar
+      /* console.log(data.results); */
+      return data.results.map((character) => {
+        /* console.log(character); */
+        return {
+          id: character.id,
+          name: character.name,
+          status: character.status,
+          species: character.species,
+          origin: character.origin.name,
+          episodes: character.episode.length,
+          gender: character.gender,
+          image: character.image,
+        };
+      });
+    });
 };
 
 export default getDataFromApi;
