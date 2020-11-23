0b16ff337f87aceab6bdb420024efa76b5edd873
IreneRuedaVega <ruedavega.irene@gmail.com>
2020-11-23 17:04:08 +0100

    Create getDataFromApi component and working in fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../stylesheets/App.scss";
+import getDataFromApi from "../services/getDataFromApi";
 
-function App() {
-  return <div>Hola mundo</div>;
-}
+console.log(getDataFromApi());
+
+const App = () => {
+  useEffect(() => {
+    console.log("Me estoy montando");
+    getDataFromApi();
+  });
+  return <></>;
+};
 
 export default App;
