c2f256e9267ee4f3f6fe275726d4e95e1064fea4
IreneRuedaVega <ruedavega.irene@gmail.com>
2020-11-23 18:24:36 +0100

    Add changes in Header.scss

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Logo from "../images/logo.png";
+import "../stylesheets/Header.scss";
 
 const Header = () => {
-  return <></>;
+  return (
+    <header className="header">
+      <div className="container">
+        <h1 className="container__title">Tu buscador de personajes de</h1>
+        <img className="container__img" src={Logo} alt="Logo de Rick y Morty" />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
