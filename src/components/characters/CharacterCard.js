19c0dcf1e380c21423c61de03406a8c4dc88c9f4
IreneRuedaVega <ruedavega.irene@gmail.com>
2020-12-10 17:53:13 +0100

    Add changes in scss in CharacterCard and CharacterList

--- src/components/characters/CharacterCard.js
+++ src/components/characters/CharacterCard.js
@@ -9,21 +9,21 @@ const CharacterCard = (props) => {
   return (
     <Link to={`/character/${props.id}`} className="link">
       <li key={props.id} className="card" id={props.id}>
-        <div className="card__box">
-          <img
-            className="card__box--img"
-            src={props.image}
-            alt={`Foto de ${props.name}`}
-          />
-        </div>
         <div className="card__description">
-          <h4 className="card__description--title">{props.name}</h4>
           <div className="card__description--species">
             <h4>{props.species}</h4>
           </div>
           <div>
             <img className="icon" src={`${dead}`} alt="" />
           </div>
+        </div>
+        <div className="card__box">
+          <img
+            className="card__box--img"
+            src={props.image}
+            alt={`Foto de ${props.name}`}
+          />
+          <h4 className="card__box--title">{props.name}</h4>
         </div>
       </li>
     </Link>
