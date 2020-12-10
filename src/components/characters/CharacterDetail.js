bf4fca02955da7e2834d7f4bf103edc47c4ce411
IreneRuedaVega <ruedavega.irene@gmail.com>
2020-12-10 21:27:26 +0100

    Add changes in CharacterDetail

--- src/components/characters/CharacterDetail.js
+++ src/components/characters/CharacterDetail.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "../../stylesheets/CharacterDetail.scss";
 import { Link } from "react-router-dom";
+import Logo2 from "../../images/rmLogo.png";
 
 const ChracterDetail = (props) => {
   return (
     <div className="modal">
+      <img className="modal__img" src={Logo2} alt="Logo de Rick y Morty" />
+      <div className="home">
+        <Link to="/" className="home__goBack">
+          <div className="home__goBack--text">Back</div>
+        </Link>
+      </div>
       <div className="characterModal">
         <img
           className="characterModal__img"
@@ -17,26 +24,31 @@ const ChracterDetail = (props) => {
           </span>
           <div className="characterModal__description--box">
             <span className="species">
-              <h4 className="species__title">Specie:</h4>
-              <p>{props.species}</p>
+              <p className="species__text">
+                <h4 className="species__text--title">Specie:</h4>
+                {props.species}
+              </p>
             </span>
             <span className="origin">
-              <h4 className="origin__title">Origin:</h4>
-              <p>{props.origin}</p>
+              <p className="origin__text">
+                <h4 className="origin__text--title">Origin:</h4>
+                {props.origin}
+              </p>
             </span>
             <span className="status">
-              <h4 className="status__title">Status:</h4>
-              <p>{props.status}</p>
+              <p className="status__text">
+                <h4 className="status__text--title">Status:</h4>
+                {props.status}
+              </p>
             </span>
             <span className="episode">
-              <h4 className="episode__title">Episodes:</h4>
-              <p>{props.episodes}</p>
+              <p className="episode__text">
+                <h4 className="episode__text--title">Episodes:</h4>
+                {props.episodes}
+              </p>
             </span>
           </div>
         </div>
-        <Link to="/" className="goBack">
-          <div className="home">Volver</div>
-        </Link>
       </div>
     </div>
   );
